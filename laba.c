7d3bed2552d01e3f150d27866b23f140ae85c100
moskovets2001 <moskovets2001@yandex.ru>
2017-06-30 17:33:35 +0300

    я думаю, это последний вариант

--- laba.c
+++ laba.c
@@ -6,43 +6,45 @@
 
 int main()
 {
-double a, b, c, d, i, i1, k;
+    double a, b, c, d, i, i1, k;
 
 
 
-k = scanf("%lf%lf%lf", &a, &b, &c);
-if(k != 3)
-{
-    printf("incorect input");
-    goto a;
-}
+    k = scanf("%lf%lf%lf", &a, &b, &c);
+    if(k != 3)
+    {
+        printf("incorect input");
+    }
+    else
+    {
 
-d = b*b - 4*c*a;
-if(fabs(d) < EPS)
-{
-    printf("tolko odno reshenie - %lf", (-b/(2*a)));
-    goto a;
-}
-if(d < 0)
-{
- printf("korney net");
- goto a;
-}
+        Beep(2000,700);
+        Beep(1900,700);
+        Beep(1800,700);
+        Beep(1700,700);
+        Beep(1600,700);
+        Beep(2100,700);
+        Beep(1600,700);
 
+        printf("rishennya priynyato:\n", i);
+        d = b*b - 4*c*a;
+        if(fabs(d) < EPS)
+        {
+            printf("tolko odno reshenie - %lf", (-b/(2*a)));
+        }
+        if(d < 0)
+        {
+            printf("korney net");
+        }
 
-i = ((-b + sqrt(d))/(2*a));
-i1 = ((-b - sqrt(d))/(2*a));
- Beep(2000,700);
- Beep(1900,700);
- Beep(1800,700);
- Beep(1700,700);
- Beep(1600,700);
- Beep(2100,700);
- Beep(1600,700);
+        i = ((-b + sqrt(d))/(2*a));
+        i1 = ((-b - sqrt(d))/(2*a));
 
-printf("rishennya priynyato:\n", i);
-printf("1 koren %lf\n", i);
-printf("2 koren %lf", i1);
-a:
-return 0;
+        if(d > 0 )
+        {
+            printf("1 koren %lf\n", i);
+            printf("2 koren %lf", i1);
+        }
+    }
+    return 0;
 }
