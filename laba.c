8f909a04100b87b80e3f1f587eeab33774c4827f
moskovets2001 <moskovets2001@yandex.ru>
2017-07-02 19:46:02 +0300

    final version

--- laba.c
+++ laba.c
@@ -7,33 +7,28 @@
 int main()
 {
     double a, b, c, d, i, i1, k;
-
-
-
     k = scanf("%lf%lf%lf", &a, &b, &c);
-    if(k != 3)
+    if (k != 3)
     {
         printf("incorect input");
     }
     else
     {
-
-        d = b*b - 4*c*a;
-        if(fabs(d) < EPS)
+        d = b * b - 4 * c * a;
+        if (fabs(d) < EPS)
         {
-            printf("tolko odno reshenie - %lf", (-b/(2*a)));
+            printf("tolko odno reshenie - %lf", (-b / (2 * a)));
         }
-        else if(d < 0)
+        else if (d < 0)
         {
             printf("korney net");
         }
         else
         {
-            i = ((-b + sqrt(d))/(2*a));
-            i1 = ((-b - sqrt(d))/(2*a));
+            i = ((-b + sqrt(d)) / (2 * a));
+            i1 = ((-b - sqrt(d)) / (2 * a));
             printf("1 koren %lf\n", i);
             printf("2 koren %lf", i1);
-
         }
     }
     return 0;
