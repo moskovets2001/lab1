e61bd452a359d3022d4d1db3076a175a689b9484
moskovets2001 <moskovets2001@yandex.ru>
2017-07-02 21:30:54 +0300

    code checkerнутая

--- laba2.c
+++ laba2.c
@@ -3,21 +3,22 @@
 
 int main()
 {
-int a[100], i, j, max = -13873, k=0;
+    int a[100], i, j, max = -13873, k = 0;
 
-scanf("%d", &a[0]);
-i = 1;
-while (scanf("%d", &a[i]) == 1)
-    i++;
-for (j = 0; j < i; j++)
-{
-    if (a[j] >= max)
+    scanf("%d", &a[0]);
+    i = 1;
+    while (scanf("%d", &a[i]) == 1)
+        i++;
+    for (j = 0; j < i; j++)
     {
-        if (a[j] > max)
-            max = a[j];
-        else k++;
+        if (a[j] >= max)
+        {
+            if (a[j] > max)
+                max = a[j];
+            else
+                k++;
+        }
     }
-}
-printf("%d", k);
-return 0;
+    printf("%d", k);
+    return 0;
 }
