3e1cffb327d197ad059a6d668173760ae27e5b2d
moskovets2001 <moskovets2001@yandex.ru>
2017-07-02 21:27:32 +0300

    замечания есть?

---
+++ laba2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main()
+{
+int a[100], i, j, max = -13873, k=0;
+
+scanf("%d", &a[0]);
+i = 1;
+while (scanf("%d", &a[i]) == 1)
+    i++;
+for (j = 0; j < i; j++)
+{
+    if (a[j] >= max)
+    {
+        if (a[j] > max)
+            max = a[j];
+        else k++;
+    }
+}
+printf("%d", k);
+return 0;
+}
